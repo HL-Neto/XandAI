80f47a6e6349a418adc9d3675038d092148220a9
HL-Neto <humbertolunaneto@gmail.com>
2025-08-09 21:59:00 -0300

    vers 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BarTrue } from './scripts';
+import { BarTrue, showBar } from './scripts';
 
 function App() {
   return (
@@ -9,14 +9,38 @@ function App() {
   <div className ="navBar">
       <nav>
         <ul>
+          <li onClick={showBar}><a href='#'>A</a></li>
+          <li><a href='#'>B</a></li>
+          <li><a href='#'>C</a></li>
+          <li><a href='#'>D</a></li>
+          
+        </ul>
+      </nav>
+    </div>
+  
+    <div className = "hisChat">
+      <nav>
+        <ul>
           <li><a href='#'>A</a></li>
-          <li onClick={BarTrue}><a href='#'>B</a></li>
+          <li><a href='#'>B</a></li>
           <li><a href='#'>C</a></li>
           <li><a href='#'>D</a></li>
         </ul>
       </nav>
+
     </div>
+  
+  
+  
+  
+  
+  
+  
   </div>
+
+
+
+  
   );
 
 
--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,16 @@
-
-
-
-export function BarTrue()
-{   
-    var state = true
-}
-
-export function showBar()
-{
-    if (state === true){
-        const navBar = document.querySelector('.navBar');
+export function showBar() {
+    const navBar = document.querySelector('.navBar');
+    const hisChat = document.querySelector('.hisChat');
+  
+    if (navBar.style.width === "250px") {
+        navBar.style.width = "50px";
+        navBar.style.height = "100vh"
+        hisChat.style.display = "none";
+    } 
+ 
+    else {
         navBar.style.width = "250px";
+        navBar.style.height = "180px"
+        hisChat.style.display = "block"
     }
 }
-
-
-
